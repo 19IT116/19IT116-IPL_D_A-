f3f56306f03fb2f6571448a358958c9a08636ada
vrundpatel017@gmail.com <vrundpatel017@gmail.com>
2023-06-21 11:43:30 +0000

    .

--- models/staging/jaffle_shop/stg_customer.sql
+++ models/staging/jaffle_shop/stg_customer.sql
@@ -1,10 +1,13 @@
 with
     customers as (
 
-        select id as customer_id, first_name, last_name 
-        from {{ source('jaffle_shop','customers') }}
+        select 
+            id as customer_id, 
+            first_name, 
+            last_name
+
+        from {{ source('jaffle_shop','customer') }}
 
 )
 
-select *
-from customers
+select * from customers
