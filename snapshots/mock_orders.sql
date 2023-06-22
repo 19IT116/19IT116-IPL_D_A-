938d1070f5b56aa1865bbeec3c77f02eb6fc3838
vrundpatel017@gmail.com <vrundpatel017@gmail.com>
2023-06-22 09:07:20 +0000

    .

--- snapshots/mock_orders.sql
+++ snapshots/mock_orders.sql
@@ -4,14 +4,14 @@
 
 {{
     config(
-        target_database='DEMO_DB'
-        target.schema=new_schema,
+        target_database='DEMO_DB',
+        target_schema=new_schema,
         unique_key='order_id',
+
         strategy='timestamp',
         updated_at='updated_at',
-    )
+    ) 
 }}
-
 
 select * from DEMO_DB.{{target.schema}}.mock_orders
 
