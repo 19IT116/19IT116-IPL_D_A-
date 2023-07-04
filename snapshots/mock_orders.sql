39d3b9684bbf4a10f2d060db14c57f98c92ad7b8
vrundpatel017@gmail.com <vrundpatel017@gmail.com>
2023-07-04 04:49:27 +0000

    .

--- snapshots/mock_orders.sql
+++ snapshots/mock_orders.sql
@@ -1,19 +1,22 @@
 
-{% snapshot orders_snapshot %}
+{% snapshot mock_orders %}
 
 {% set new_schema = target.schema + '_snapshot' %} 
 
 {{
     config(
-      target_database='DEMO_DB',
+      target_database='DEMO_DB', 
       target_schema=new_schema,
-      unique_key='id',
+      unique_key='order_id',
 
       strategy='timestamp',
       updated_at='updated_at',
     )
 }}
 
-select * from {{ source('jaffle_shop', 'orders') }}
+select * from DEMO_DB.{{target.schema}}.mock_orders
+
+-- select * from {{ source('jaffle_shop', 'orders') }}
+
 
 {% endsnapshot %}
